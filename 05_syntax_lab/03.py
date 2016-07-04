dbc15c6b1288d2447bbd74594f69ba22da9df85d
Guyemin <guyemin@gmail.com>
2016-07-04 13:31:48 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -1,7 +1,11 @@
-"""
-Write a program that randomizes a number
-and prints the sum total of its digits.
-For example if the number was: 2345
-The result should be: 14
-"""
-
+from random import randint
+sum = 0
+x = randint(1,10000)
+print "The number is %s" % x
+while (x % 10) > 0:
+	num = x % 10
+	print num
+	sum = sum + num
+	x = x / 10
+	
+print "The sum is: %s" % sum
