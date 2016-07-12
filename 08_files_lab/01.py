82ae25d889512312935db4698d03081c1efb8c36
Guyemin <guyemin@gmail.com>
2016-07-12 07:25:50 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,5 +1,5 @@
 import sys
-with open (sys.argv[1], "r") as fin:
-	with open (sys.argv[2], "w") as fout:
-		for line in fin:
-			fout.write(line)
+ip= {'work':'10.0.0.2', 'router':'10.0.0.1', 'mycard': '10.0.0.5', 'router':'10.0.60.6'}
+for arg in sys.argv:
+	if arg in ip:
+		print ip[arg]
