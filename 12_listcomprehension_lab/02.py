7092afaf3413ae3c0fe59588204be1622eb45d12
Guyemin <guyemin@gmail.com>
2016-07-14 09:23:30 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,10 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
+import sys 
+import os
 
+a = range (97,123)
+b = range (97,123)
+c = range (97,123)
+
+letters = (range(97,123))
+match = [chr(x)+chr(y)+chr(z) for x in letters for y in letters for z in letters]
+print match
